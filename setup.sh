e01acd61c8db38956abf34558667da7a1a979035
paulruvolo <paullundyruvolo@gmail.com>
2016-10-23 16:07:45 -0400

    Created a script to wrapper the invocation of docker

--- setup.sh
+++ setup.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
+echo "#!/bin/bash" > ./connect
+echo "docker run --net=host -e HOST=\$1 -it paulruvolo/neato_docker" >> ./connect
+
 /c/Program\ Files/Oracle/VirtualBox/VBoxManage.exe controlvm default natpf1 "gstneato,udp,,5000,,5000"
 docker pull paulruvolo/neato_docker
